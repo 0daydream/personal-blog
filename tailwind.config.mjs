aea802d2f2b1182b4e64947421f9a4590c945a5c
heaven <heaven@heaven.heaven>
2025-04-28 20:44:21 +0200

    Added vars color quick theme change

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,13 @@ export default {
 				unifont: ['Unifont', 'sans-serif'], // Use Unifont as the default sans font
 				gohu: ['Gohu', 'sans-serif'], // Use Unifont as the default sans font
 			},
+			colors: {
+				'c_mauve': '#f5c2e7',
+				'c_crust': '#11111b',
+				'c_text': '#cdd6f4',
+				'c_surface0': '#313244',
+			},
+
 		},
 	},
 	plugins: [
