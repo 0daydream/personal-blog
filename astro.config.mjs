016da7822cc255a6f978390a1a7b29c754e658d8
heaven <heaven@heaven.heaven>
2025-05-14 19:18:49 +0200

    added rss and small overhaul :3

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://vxdev.xyz",
+  site: "https://ntcall.de",
   markdown: {
     shikiConfig: {
       theme: 'catppuccin-mocha',
